4e7ccd82698015377a767d8e5545555611edf2ea
danya7125 <dany.melinkov@gmail.com>
2026-09-12 13:15:19 +0300

    докер

--- app.py
+++ app.py
@@ -5,8 +5,10 @@ app = create_app()
 
 
 if __name__ == "__main__":
-    with app.app_context():
-        check_connection()
-        print("PostgreSQL: подключение успешно")
+    app.run(
+        host="0.0.0.0",
+        port=5000,
+        debug=False,
+    )
 
     app.run(debug=app.config.get("DEBUG", False))
